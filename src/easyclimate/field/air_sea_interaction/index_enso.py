fix: calculation of ENSO index
drop `drop_vars("month")` and fix running mean error

src/easyclimate/field/air_sea_interaction/index_enso.py
@@ -2,14 +2,10 @@
 ENSO Indices
 """
 
-from ...core.utility import (
-    sort_ascending_latlon_coordinates,
-    generate_dataset_dispatcher,
-)
+from ...core.utility import sort_ascending_latlon_coordinates
 import xarray as xr
 
 
-@generate_dataset_dispatcher
 def calc_index_nino1and2(
     sst_monthly_anomaly_data: xr.DataArray | xr.Dataset,
     lat_dim: str = "lat",
@@ -47,7 +43,7 @@ def calc_index_nino1and2(
         {lat_dim: slice(-10, 0), lon_dim: slice(270, 280)}
     ).mean(dim=(lat_dim, lon_dim))
     nino1plus2_index.name = "Nino1+2_index"
-    return nino1plus2_index.drop_vars("month")
+    return nino1plus2_index
 
 
 def calc_index_nino3(
@@ -88,7 +84,7 @@ def calc_index_nino3(
         {lat_dim: slice(-5, 5), lon_dim: slice(210, 270)}
     ).mean(dim=(lat_dim, lon_dim))
     nino3_index.name = "Nino3_index"
-    return nino3_index.drop_vars("month")
+    return nino3_index
 
 
 def calc_index_nino34(
@@ -135,10 +131,12 @@ def calc_index_nino34(
         nino34_index = nino34_index.mean(dim=(lat_dim, lon_dim))
     else:
         nino34_index = (
-            nino34_index.rolling(time=running_mean).mean().mean(dim=(lat_dim, lon_dim))
+            nino34_index.rolling(time=running_mean, center=True)
+            .mean()
+            .mean(dim=(lat_dim, lon_dim))
         )
     nino34_index.name = "Nino34_index"
-    return nino34_index.drop_vars("month")
+    return nino34_index
 
 
 def calc_index_OMI(
@@ -185,10 +183,12 @@ def calc_index_OMI(
         omi_index = omi_index.mean(dim=(lat_dim, lon_dim))
     else:
         omi_index = (
-            omi_index.rolling(time=running_mean).mean().mean(dim=(lat_dim, lon_dim))
+            omi_index.rolling(time=running_mean, center=True)
+            .mean()
+            .mean(dim=(lat_dim, lon_dim))
         )
     omi_index.name = "Oceanic_Nino_Index_index"
-    return omi_index.drop_vars("month")
+    return omi_index
 
 
 def calc_index_nino4(
@@ -227,4 +227,4 @@ def calc_index_nino4(
         {lat_dim: slice(-5, 5), lon_dim: slice(160, 210)}
     ).mean(dim=(lat_dim, lon_dim))
     nino4_index.name = "Nino4_index"
-    return nino4_index.drop_vars("month")
+    return nino4_index