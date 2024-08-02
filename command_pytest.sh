84aa1262cc205126523671d1df8ac8bf2930a9e5
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2024-08-02 10:57:53 +0800

    fix: Add a shebang or a 'shell' directive

--- command_pytest.sh
+++ command_pytest.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 pytest --mpl --mpl-baseline-path="test/baseline_images" --cov src
 pre-commit run --all-files
--- docs/clean.sh
+++ docs/clean.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 rm -r ".\source\auto_gallery_output\*"
 rm ".\source\api_index\generated\*"
 rm ".\source\sg_execution_times.rst"
