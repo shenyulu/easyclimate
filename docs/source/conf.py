a9d451b33fa8ed130735138a5dfe8c20d86cf5e6
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2026-03-18 21:44:42 +0800

    fix: jupyter_sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     "recommonmark",
     "sphinx_markdown_tables",
     "sphinx.ext.mathjax",
-    "jupyter_sphinx",
+    # "jupyter_sphinx",
     # Add inline tabbed content to your Sphinx documentation
     "sphinx_inline_tabs",
     "sphinx_gallery.gen_gallery",
